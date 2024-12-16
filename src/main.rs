78d5a9c18fd20471f9225bb64f9d0124d4b4bf3b
Michael X. Grey <greyxmike@gmail.com>
2024-12-16 22:30:35 +0800

    Demonstrate creating the advanced executor
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,9 @@ use do_nothing_advanced::*;
 fn main() {
     let mut executor: Executor = Context::default().create_executor(DoNothingRuntime);
     executor.spin(SpinOptions::default());
+
+    let mut executor: Executor = Context::default().create_do_nothing_executor(
+        DoNothingOptions { how_many_nothings: 10 }
+    );
+    executor.spin(SpinOptions::spin_once());
 }
